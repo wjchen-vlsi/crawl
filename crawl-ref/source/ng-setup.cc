c11ec148dd6ab1337d392d442abbb3aae5c87a24
Aidan Holm <aidanholm@gmail.com>
2018-09-02 23:25:39 +0800

    Add species/background newgame doll generator
    This adds a helper function to generate dolls for character combos.
    This is done in a rather ugly fashion by (partially) generating
    the new game character. A small segment of the character setup
    code in _setup_generic() that's relevant to initial equipment is
    duplicated, with some minor changes; this code modifies the global `you`
    variable, and fill_doll_equipment() is then called to generate the doll
    corresponding to the character in `you`.
    
    Regarding the modified character generation code, everything after
    give_items_skills() is removed, since that's what determines a player's
    starting equipment, and thus the doll appearance. An exception is
    the item colour randomization. Separately, _unfocus_stats() was not
    included, since I'm reasonably sure that this has no effect on initial
    item generation, and I didn't want to make it non-static.

--- crawl-ref/source/ng-setup.cc
+++ crawl-ref/source/ng-setup.cc
@@ -232,7 +232,7 @@ static void _give_ammo(weapon_type weapon, int plus)
     }
 }
 
-static void _give_items_skills(const newgame_def& ng)
+void give_items_skills(const newgame_def& ng)
 {
     switch (you.char_class)
     {
@@ -506,7 +506,7 @@ static void _setup_generic(const newgame_def& ng)
     give_basic_mutations(you.species);
 
     // This function depends on stats and mutations being finalised.
-    _give_items_skills(ng);
+    give_items_skills(ng);
 
     if (you.species == SP_DEMONSPAWN)
         roll_demonspawn_mutations();
